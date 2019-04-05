3088438ddadcab90fac395bd6aaea5f3b9e09d31
lb <lori.bordzuk@gmail.com>
2019-04-05 13:32:52 -0700

    complete final merging and train test split py file

--- src/import_train_test_split.py
+++ src/import_train_test_split.py
@@ -6,6 +6,7 @@ import string
 import requests
 from bs4 import BeautifulSoup
 
+from sklearn.model_selection import train_test_split
 from _import_data import *
 
 
@@ -53,17 +54,21 @@ for df_name in feat_dfs.keys():
     merged_df = _merge_on_fips(merged_df, df_name, feats_fips_colnames)
 
 
-#  --- INITAL SCRUBBING   ---
+#  ----- INITAL SCRUBBING  ------
 
 #removing Puerto Rico --> not including in analysis, too many features missing
 merged_df = merged_df[merged_df['State_master'] != "Puerto Rico"]
 
-#removing rows without diabetes 2013 info (= no target information)
+#removing rows without diabetes 2013 info (i.e. missing target information)
 merged_df = merged_df[merged_df['DB:2013:percent'].isnull() == False]
 
+#create target
+merged_df['Diabetes_pct_growth_rate'] = ((merged_df["DB:2013:percent"] - merged_df["DB:2010:percent"]) *100) / merged_df["DB:2010:percent"]
+
 #removing duplicative columns
-desired_merged_columns = ['State_master', 'FIPS_master', 'County_master', 'DB:2009:percent',
-       'DB:2010:percent', 'DB:2013:percent', 'TOT_POP', 'TOT_MALE', 'WAC_MALE', 'WAC_FEMALE', 'BAC_MALE',
+#including
+desired_merged_columns = ['State_master', 'FIPS_master', 'County_master', 'DB:2009:percent', 'DB:2010:percent', 'TOT_POP',
+        'TOT_MALE', 'WAC_MALE', 'WAC_FEMALE', 'BAC_MALE',
        'BAC_FEMALE', 'IAC_MALE', 'IAC_FEMALE', 'AAC_MALE', 'AAC_FEMALE',
        'NAC_MALE', 'NAC_FEMALE', 'H_MALE', 'H_FEMALE', 'AgeGrp01:0-4:2010',
        'AgeGrp02:5-9:2010', 'AgeGrp03:10-14:2010', 'AgeGrp04:15-19:2010',
@@ -74,21 +79,35 @@ desired_merged_columns = ['State_master', 'FIPS_master', 'County_master', 'DB:20
        'AgeGrp17:80-84:2010', 'AgeGrp18:85+:2010', 'UnemploymentRate:2010',
        'PCT_LACCESS_POP10', 'FFRPTH09', 'Poverty_Rate_2010',
        'LI:2009:percent', 'LI:2010:percent',  'OB:2009:percent', 'OB:2010:percent',
-        'Rural_percent_2010', 'Alcohol:Any:2010', 'Alcohol:Heavy:2010']
+        'Rural_percent_2010', 'Alcohol:Any:2010', 'Alcohol:Heavy:2010', 'Diabetes_pct_growth_rate']
 
 merged_df = merged_df[desired_merged_columns]
+
+#export all final data (to be used for visualizations, plotting, etc)
+merged_df.to_csv('../data/ALL_MERGED_DATA.csv')
 
 
 #  --- TRAIN TEST  SPLIT  ---
 
+X = merged_df.drop(['Diabetes_pct_growth_rate','State_master', 'FIPS_master', 'County_master'], axis = 1)
+y = merged_df['Diabetes_pct_growth_rate']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=30)
+
+#export all training and test sets. Only training to be used for model selection and fitting.
+X_train.to_csv('../data/X_train.csv')
+X_test.to_csv('../data/X_test.csv')
+y_train.to_csv('../data/y_train.csv')
+y_test.to_csv('../data/y_test.csv')
 
 
 
 '''
 
 LATER:
+#CHANGE DIABETES GROWTH TO --> % POPULATION THAT IS NEW TO DIABETES? (i.e. total diabetes pop #s?)
+
 #per second pass, adding Diabetes growth rate TARGET
-third_pass['Diabetes_pct_growth_rate'] = (third_pass["DDP:2013:percent.9"] - third_pass["DDP:2010:percent.6"]) / third_pass["DDP:2010:percent.6"]
 
 #new for third pass: adding percent increase 2009-2010
 third_pass['DDP:2009-2010:Growth_Rate'] = (third_pass["DDP:2010:percent.6"] - third_pass["DDP:2009:percent.5"]) / third_pass["DDP:2009:percent.5"]
