cd917ade9d2b5724b25effe3dd923629b3ed8cb8
lb <lori.bordzuk@gmail.com>
2019-04-07 13:18:00 -0700

    created final class for lasso and random forest modelling

--- src/model.py
+++ src/model.py
@@ -1,54 +1,53 @@
 import pandas as pd
 import numpy as np
 
-from _data_cleaning import *
-
-
-filepath_X = '../data/X_train.csv'
-X_train = clean_reformat_features(filepath_X)
-filepath_y = '../data/y_train.csv'
-y_train = pd.read_csv(filepath_y)
-
-
 from sklearn.preprocessing import StandardScaler
-
-from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Lasso
-from sklearn.linear_model import Ridge
-
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.ensemble import GradientBoostingRegressor
-
-from sklearn.model_selection import GridSearchCV
-
-from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_squared_error
-
-import statsmodels.api as sm
-#import statsmodels.stats.api as sms
-#from statsmodels.compat import lzip
-from statsmodels.regression.linear_model import OLS #.fit_regularized
 
 
 class Diabetes_Rate_Model():
 
     def __init__(self):
         self.scalar = StandardScaler()
-        self.model =  Ridge(alpha = 10)
+        self.modellinear =  Lasso(alpha =  .5, max_iter = 1000)
+        self.modelensemble = RandomForestRegressor(max_depth = 20, min_samples_leaf = 5, n_estimators = 1000)
+        self.mse_avg = None
 
-    def fit(self, X_train, y_train):
+    def fit_linear(self, X_train, y_train):
         self.X_train = X_train
         self.y_train = y_train
 
         self.scalar.fit(X_train)
         self.X_train_scaled = self.scalar.transform(X_train)
-        self.model.fit(self.X_train_scaled, self.y_train)
+        self.modellinear.fit(self.X_train_scaled, self.y_train)
+        if not self.mse_avg:
+            yavg = self.y_train.mean()
+            y_pred_avg = np.array([yavg] * self.X_train.shape[0])
+            self.mse_avg = mean_squared_error(y_train, y_pred_avg)
 
-    def predict(self, X):
+
+    def predict_linear(self, X):
         X_scaled = self.scalar.transform(X)
-        return self.model.predict(X_scaled)
+        return self.modellinear.predict(X_scaled)
 
-#    mse(y_true, y_pred):
-#        return mean_squared_error(y_true,
+
+    def fit_ensemble(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.modelensemble.fit(self.X_train, self.y_train)
+        if not self.mse_avg:
+            yavg = self.y_train.mean()
+            y_pred_avg = np.array([yavg] * self.X_train.shape[0])
+            self.mse_avg = mean_squared_error(y_train, y_pred_avg)
+
+    def predict_ensemble(self, X):
+        return self.modelensemble.predict(X)
+
+    def mse(self, y_true, y_pred):
+        return mean_squared_error(y_true, y_pred)
+
+    def mse_pct_improvement(self, y_true, y_pred):
+        mse = mean_squared_error(y_true, y_pred)
+        return (mse - self.mse_avg) / self.mse_avg
